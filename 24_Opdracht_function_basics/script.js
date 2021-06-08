eab2edc76ecc51094b5e8ac6752d24f609bbcdcd
Romy <80348985+ROOMH@users.noreply.github.com>
2021-06-08 11:08:49 +0200

    Add files via upload

---
+++ 24_Opdracht_function_basics/script.js
@@ -0,0 +1,23 @@
+const pizzaMaking = function () {
+    console.log("Deeg maken");
+    console.log("2 uur laten rijzen");
+    console.log("Pizzasaus maken");
+    console.log("Pizza beleg snijden");
+    console.log("Bodem uitrollen en saus en beleg toevoegen");
+    console.log("Pizaa 20 minuten in de oven");
+};
+
+const SushiMaking = function () {
+    console.log("Sushirijst koken");
+    console.log("Rijst in mandje afkoelen");
+    console.log("Snij de vis en groentes");
+    console.log("Sushi rollen");
+    console.log("Sushi rollen snijden");
+}
+
+SushiMaking();
+SushiMaking();
+SushiMaking();
+
+pizzaMaking();
+pizzaMaking();
